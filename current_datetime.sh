b32ad06716166aaa58ab5779a7e863e716506a7e
AbdoulRaouf2005 <abdoulraoufharouna62@gmail.com>
2025-05-08 01:46:29 +0100

    Update current_datetime.sh

--- current_datetime.sh
+++ current_datetime.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo $(date +"%Y-%m-%d__%H:%M:%S) 			#affichage de la date grace à la commande date et de ces formats
+echo $(date +%Y-%m-%d) $(date +%H:%M:%S) 			#affichage de la date grace à la commande date et de ces formats
 exit 0                                  #mettre fin au programme 
