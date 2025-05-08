68e53e7cea96044e4bd0cef58ea5baea01ece227
AbdoulRaouf2005 <abdoulraoufharouna62@gmail.com>
2025-05-08 11:15:45 +0100

    Add files via upload

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-fichier='Sample.txt'						#affectation de la valeur mis par l'utilisateur
-if [ -f "$fichier" ]; then                             #verification de l'existence du fichier
-      echo "Le fichier '${fichier}' existe."           #si le fichier existe affichage du message
-      exit 0                                           #mettre fin au programme
-else                                                   #dans le cas ou la condition de if est fausse
-      echo "Le fichier '${fichier}' n'existe pas."     #affiche un message egalement
-      exit 0                                           #mettre fin au programme
-fi                                                     # fin des blocs if...else
+
+#invite utilisateur a entré le nom du fichier
+echo "Entrez le nom du fichier :"
+read filename
+
+#verification du fichier
+
+if [ -f "$filename" ]; then
+echo "Le fichier '$filename' existe."
+else
+echo "Le fichier '$filename' n'existe pas."
+fi
+
+exit 0
