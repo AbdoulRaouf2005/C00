96feb6f1a91be1f4d5dd5927b9f4183fca161719
AbdoulRaouf2005 <abdoulraoufharouna62@gmail.com>
2025-05-08 09:48:47 +0100

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -2,7 +2,8 @@
 read -p "Entrez le nom du fichier : " fichier           #affectation de la valeur mis par l'utilisateur
 if [ -f "$fichier" ]; then                             #verification de l'existence du fichier
       echo "Le fichier '${fichier}' existe."           #si le fichier existe affichage du message
+      exit 0                                           #mettre fin au programme
 else                                                   #dans le cas ou la condition de if est fausse
       echo "Le fichier '${fichier}' n'existe pas."     #affiche un message egalement
+      exit 0                                           #mettre fin au programme
 fi                                                     # fin des blocs if...else
-exit 0                                                 #mettre fin au programme
